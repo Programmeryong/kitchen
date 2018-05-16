5b1d5ab1616b3e5fcf9958855d6c6a995d4b4fc4
Hwenze <36119371+Hwenze@users.noreply.github.com>
2018-05-16 20:31:07 +0800

    app1.2
    增加了收藏效果和关注效果！

--- 小厨房/html/js/Othershomepage.js
+++ 小厨房/html/js/Othershomepage.js
@@ -22,9 +22,26 @@ $(function(){
 				{img:'./img/xwc.jpg'},
 				{img:'./img/ms.jpg'},
 			],
+			list1:[
+				{guanzhu:'关注'},
+			],
 			currentTab: '作品18',
-            tabs: ['菜谱0', '作品18']
+            tabs: ['菜谱0', '作品18'],
+            show:false,
 		},
+		methods:{
+			guanzhu:function(){
+				this.show=!this.show;
+				// console.log(this.list1[index]);
+				if(this.show==true){
+					this.list1[0].guanzhu='已关注';
+					$('.gz').css({background:'#ccc'});
+				}else{
+					this.list1[0].guanzhu='关注';
+					$('.gz').css({background:'#FF5733'});
+				}
+			}
+        },
 		computed: {
             currentTabComponent: function () {
                 return 'tab-' + this.currentTab.toLowerCase()
